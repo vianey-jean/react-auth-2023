45f2067cab67c789e73d98fd31fb34d897945737
vianey.jean <vianey.jean@ymail.com>
2023-02-28 00:08:02 +0400

    First commit

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCo6DNv9HiayjswPhH614QMLgBjhsKYLic",
+  authDomain: "react-firebase-auth-v2601.firebaseapp.com",
+  projectId: "react-firebase-auth-v2601",
+  storageBucket: "react-firebase-auth-v2601.appspot.com",
+  messagingSenderId: "449712738588",
+  appId: "1:449712738588:web:44e08319bea9af8abfaad9"
 };
 
 const app = initializeApp(firebaseConfig);
